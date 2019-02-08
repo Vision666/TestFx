316e336f7e8817b32c1f54f4643273dc49313327
Vision666 <bvision666@gmail.com>
2019-02-08 18:22:59 +0300

    JavaFx training

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,44 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private Label testLabel;
+
+    @FXML
+    private Button fullscreenButton;
+
+    @FXML
+    void initialize() {
+        closeButton.setOnAction(event -> {
+            System.out.println("closeButton pressed");
+            closeButtonAction();
+        });
+    }
+
+    @FXML
+    private void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,22 +2,51 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init(){
+        System.out.println("В методе init");
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        //primary stage configuration
         primaryStage.setTitle("TestFX");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setOpacity(1.0);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        //primaryStage.setResizable(false);
+
+        Text textDem = new Text("Text demonstration");
+        textDem.setLayoutX(0);
+        textDem.setLayoutY(100);
+        Group group = new Group(textDem);
+
+        primaryStage.setScene(new Scene(root, 800, 600));
+
+
         primaryStage.show();
+        System.out.println("В теле метода start");
+
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop(){
+        System.out.println("В теле метода stop");
     }
 }
